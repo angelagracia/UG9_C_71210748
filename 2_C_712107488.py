756d416bce568ec9dffeab226881cfc922925fe3
angelagracia <93326534+angelagracia@users.noreply.github.com>
2021-11-12 06:38:00 +0700

    Add files via upload

---
+++ 2_C_712107488.py
@@ -0,0 +1,18 @@
+#SOAL NO 2 a
+
+string = input("Masukkan kalimat : ")
+
+substring = input("Masukkan kata untuk dihitung: ")
+
+count = string.count(substring)
+
+print('ada', count, 'buah kata', substring)
+
+
+
+#SOAL NO 2 b
+
+string = input("Masukkan kalimat : ")
+substring = input ("Masukkan kata untuk dihitung: ")
+count = string.count(substring)
+print('ada', count, 'buah kata', substring)
