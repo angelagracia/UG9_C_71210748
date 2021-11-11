59dd1d9b8905d5ba00244fcd25ad13358a3c4960
angelagracia <93326534+angelagracia@users.noreply.github.com>
2021-11-12 06:36:59 +0700

    Add files via upload

---
+++ 1_C_71210748.py
@@ -0,0 +1,17 @@
+#SOAL NO 1 a
+#rumus : phi x r^2
+
+phi = 3.14
+r = int(input("Masukkan jari-jari : "))
+luas = phi * ( r * r )
+
+print (luas)
+
+#SOAL NO 1 b
+#rumus : phi x r^2
+
+phi = 3.14
+r = int(input("Masukkan jari-jari : "))
+luas = phi * ( r * r )
+
+print (luas)
