970f2cf3168404352fd83c85474f4a9a8498f73c
angelagracia <93326534+angelagracia@users.noreply.github.com>
2021-11-12 07:38:53 +0700

    Add files via upload

---
+++ 3_C_71210748.py
@@ -0,0 +1,22 @@
+#SOAL 3a
+
+a = int(input("Masukkan alas atap: "))
+ta = int(input("Masukkan tinggi atap: "))
+tt = int(input("Masukkan tinggi tembok: "))
+
+luas = (a * tt * 0.5) + (tt * tt)
+semen = luas / 5
+
+print("rumah tersebut membutuhkan", semen, "sak semen")
+
+#Soal 3b
+a = int(input("Masukkan alas atap: "))
+ta = int(input("Masukkan tinggi atap: "))
+tt = int(input("Masukkan tinggi tembok: "))
+
+luas = (a * tt * 0.5) + (tt * tt)
+semen = luas / 5
+
+print("rumah tersebut membutuhkan", semen, "sak semen")
+
+        
